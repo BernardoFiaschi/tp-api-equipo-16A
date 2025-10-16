0af7e79398aa00fb6e90237e98c7e07143988887
Florencia <117230347+florolivera@users.noreply.github.com>
2025-10-16 18:20:26 -0300

    dlt archivos que no se usan

--- DTOs/ImagenCreateDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace tp_api_equipo_16A.DTOs
-{
-    public class ImagenCreateDTO
-    {
-        public List<string> Imagenes { get; set; } = new List<string>(); 
-    }
-}
--- DTOs/ItemRef.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace tp_api_equipo_16A.DTOs
-{
-    public class ItemRef
-    {
-        public int Id { get; set; }
-        public string? Descripcion { get; set; }
-    }
-}
