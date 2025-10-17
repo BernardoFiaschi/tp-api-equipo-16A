bc2fdf0b0997acb845ad521609290d01a53cd162
Bernardo Fiaschi <162641863+BernardoFiaschi@users.noreply.github.com>
2025-10-16 22:59:49 -0300

    fix carpeta incorrecta

---
+++ web-api-catalogo/Models/AgregarImagenesDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace web_api_catalogo.Models
+{
+    public class AgregarImagenesDto
+    {
+        public int IdArticulo { get; set; }
+        public List<string> ImagenesUrls { get; set; }
+    }
+}
